ef2b068047c0ce9a937734a2c20add0af5105ae0
Mohd Ziya <iamziya53@gmail.com>
2022-06-20 16:51:31 -0400

    Factorial program

--- factorial.js
+++ factorial.js
@@ -1,15 +1,15 @@
 let n=parseInt(process.argv[2]);
 let fact=1;
-/*
+
 for(let i=1; i<=n;i++){
     fact=fact*i;
 }
 console.log(fact);
-*/
 
-let i=1;
-while(i<=n){
-    fact=fact*i;
-    i++;
-}
-console.log(fact);
+
+// let i=1;
+// while(i<=n){
+//     fact=fact*i;
+//     i++;
+// }
+// console.log(fact);
