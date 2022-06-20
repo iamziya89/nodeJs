7917967800727e4dfde6dbb6ad499fc936223586
Mohd Ziya <iamziya53@gmail.com>
2022-06-20 16:44:33 -0400

    Modified code

--- classques1.js
+++ classques1.js
@@ -1,14 +1,14 @@
 let n=parseInt(process.argv[2]);
-/*
+
 for(let i=1; i<=n;i++){
     console.log(i)
 }
-*/
+
 
 // Usimg while loops
 
-let i=1;
-while(i<=n){
-console.log(i);
-i++;
-}
+// let i=1;
+// while(i<=n){
+// console.log(i);
+// i++;
+// }
