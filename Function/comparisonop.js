1749ba5fc2764807a592623a5b7cd37cdbb52f03
Mohd Ziya <iamziya53@gmail.com>
2022-06-25 13:37:11 -0400

    comparison operator

--- Function/comparisonop.js
+++ Function/comparisonop.js
@@ -4,6 +4,28 @@
 // console.log(a==b);
 
 //.........Strict Equality Operator......
-var a=5;
-var b=6;
-console.log(a==b);
+// var a=5;
+// var b=6;
+// console.log(a===b);
+// var a=5;
+// var b=5;
+// console.log(a===b);
+
+// let a=5;
+// let b="5";
+// console.log(a===b);
+
+// let a=5;
+// let b=6;
+// console.log(a!=b);
+
+let a=6;
+let b=7;
+console.log(a>=b);
+console.log(a<=b);
+
+
+
+
+
+
