16e010a327b34a1f67ef29ba7d3b8465263dafaf
Mohd Ziya <iamziya53@gmail.com>
2022-06-25 14:00:48 -0400

    practice on assignment and logical operator

---
+++ Function/Assignmentop.js
@@ -0,0 +1,29 @@
+// var x=10;
+// var y=15;
+// y=x;
+// console.log(y);
+
+//.....Addition Assignment operator
+
+// x+=1;
+// console.log(x);
+
+// y-=1;
+// console.log(y);
+
+// y*=2;
+// console.log(y);
+
+// y/=3;
+// console.log(y);
+
+
+// y%=4;
+// console.log(y);
+
+
+//..........Ternary Operator............
+
+var a=10;
+var b=15;
+(a>b) ? console.log(a+" is greater than "+b) : console.log(a+" is less than "+b);
---
+++ Function/logicalop.js
@@ -0,0 +1,10 @@
+//............AND OPERATOR............
+
+var a=5;
+var b=6;
+// console.log((a!=b)&&(a<b));
+// console.log((a==b)&&(a<b));
+
+//............OR OPERATOR............
+console.log((a!=b)||(a<b));
+console.log((a==b)||(a>b));
